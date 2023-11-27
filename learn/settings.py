7028a696785f0a778292ef6408692aca4c32507a
afiovinicius <afiovinicius@gmail.com>
2023-11-26 23:22:17 -0300

    libs 3

--- learn/settings.py
+++ learn/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", '127.0.0.1', '.vercel.app']
 
