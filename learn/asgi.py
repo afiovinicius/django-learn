eaeaf05d3f8170d8c22d8fdffce08be19ff2ea25
afiovinicius <afiovinicius@gmail.com>
2023-11-27 01:38:00 -0300

    rename function application

--- learn/asgi.py
+++ learn/asgi.py
@@ -4,4 +4,6 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learn.settings")
 
-app = get_asgi_application()
+application = get_asgi_application()
+
+app = application
