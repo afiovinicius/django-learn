2a3d2351bfd567665d658c4664e51753e59b4fca
afiovinicius <afiovinicius@gmail.com>
2023-11-26 23:06:14 -0300

    gunicorn 2

--- gunicorn_config.py
+++
@@ -1,7 +0,0 @@
-# Arquivo de configuração do Gunicorn
-
-# Número de processos de trabalho (ajuste conforme necessário)
-workers = 2
-
-# Endereço e porta para vincular (não altere a variável $PORT)
-bind = "0.0.0.0:${PORT}"
--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Certifique-se de ajustar os caminhos conforme necessário
-PROJECT_DIR="django-learn"
-APP_DIR="learn"
-GUNICORN_CONFIG="gunicorn_config.py"
+gunicorn learn.wsgi:application --workers=2 --bind 0.0.0.0:$PORT
 
-# Inicie o Gunicorn apontando para o arquivo wsgi.py
-gunicorn ${APP_DIR}.wsgi:application -c ${PROJECT_DIR}/${GUNICORN_CONFIG}
