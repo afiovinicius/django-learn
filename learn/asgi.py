d3277ca27fd4f1c7ff4f1e1b63e7ca172ce45491
afiovinicius <afiovinicius@gmail.com>
2023-12-26 11:40:06 -0300

    feat: configurando asgi teste

--- learn/asgi.py
+++ learn/asgi.py
@@ -9,10 +9,26 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learn.settings")
 
 
-http_application = get_asgi_application()
+# http_application = get_asgi_application()
 
 
-websocket_router = ProtocolTypeRouter({
+# websocket_router = ProtocolTypeRouter({
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             [
+#                 path("ws/cursor/", CursorConsumer.as_asgi()),
+#             ]
+#         )
+#     ),
+# })
+
+# application = ProtocolTypeRouter({
+#     "http": http_application,
+#     "websocket": websocket_router,
+# })
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             [
@@ -20,11 +36,6 @@ websocket_router = ProtocolTypeRouter({
             ]
         )
     ),
-})
-
-application = ProtocolTypeRouter({
-    "http": http_application,
-    "websocket": websocket_router,
 })
 
 
