00a9e399a86f5ed32c0853e8cb976a36fe9c7476
afiovinicius <afiovinicius@gmail.com>
2023-12-26 11:51:41 -0300

    feat: configuracao do websocket authmiddleware

--- learn/asgi.py
+++ learn/asgi.py
@@ -9,26 +9,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learn.settings")
 
 
-# http_application = get_asgi_application()
+http_application = get_asgi_application()
 
 
-# websocket_router = ProtocolTypeRouter({
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             [
-#                 path("ws/cursor/", CursorConsumer.as_asgi()),
-#             ]
-#         )
-#     ),
-# })
-
-# application = ProtocolTypeRouter({
-#     "http": http_application,
-#     "websocket": websocket_router,
-# })
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+websocket_router = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter(
             [
@@ -37,6 +21,22 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
+application = ProtocolTypeRouter({
+    "http": http_application,
+    "websocket": websocket_router,
+})
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             [
+#                 path("ws/cursor/", CursorConsumer.as_asgi()),
+#             ]
+#         )
+#     ),
+# })
 
 
 app = application
