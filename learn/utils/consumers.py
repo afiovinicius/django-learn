9f7955e4ff496d9385da08f37638d9133d87d578
afiovinicius <afiovinicius@gmail.com>
2023-12-27 01:17:48 -0300

    refractor: ajustes no consumers para multiplos users

--- learn/utils/consumers.py
+++ learn/utils/consumers.py
@@ -1,19 +1,31 @@
 import json
+
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.layers import InMemoryChannelLayer
 
 
 class CursorConsumer(AsyncWebsocketConsumer):
+    channel_layer: InMemoryChannelLayer
+
     async def connect(self):
+        await self.channel_layer.group_add("cursors_group", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_discard("cursors_group", self.channel_name)
 
     async def receive(self, text_data):
-        print(f"Received message: {text_data}")
         cursor_data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            "cursors_group",
+            {
+                'type': 'handle.cursor.move',
+                'content': cursor_data,
+            }
+        )
 
-        await self.send(text_data=json.dumps({
-            'type': 'cursor.move',
-            'cursor_data': cursor_data,
-        }))
+    async def handle_cursor_move(self, event):
+        cursor_data = event['content']
+
+        await self.send(text_data=json.dumps(cursor_data))
